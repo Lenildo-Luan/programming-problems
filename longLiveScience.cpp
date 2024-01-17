762bdbf1382a4247fcfe724500d874d41dc37596
Lenildo Luan <lenildoluan@gmail.com>
2024-01-17 11:06:21 -0300

    Create longLiveScience.cpp

---
+++ longLiveScience.cpp
@@ -0,0 +1,20 @@
+//1. Try to find an equation to resolve this problem in o(1)
+//2. Compound interest can resolve this
+//3. Dont't work because the problem require the numbers being
+//   truncate every multiplication
+//4. Create a loop to made this
+
+#include <stdio.h>
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+int main() {
+    string opnion;
+    
+    cin >> opnion;
+    cout << "Nao se refuta ciencia com opiniao" << endl;  
+    
+    return 0;
+}
